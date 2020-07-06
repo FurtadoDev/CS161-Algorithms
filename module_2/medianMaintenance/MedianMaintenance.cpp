be4a923ee92d631b02ff4002750a947834e4769f
Veinstin Furtado <45301868+FurtadoDev@users.noreply.github.com>
2020-07-06 05:20:31 -0230

    Completed the Median Maintenance Algorithm

--- module_2/medianMaintenance/MedianMaintenance.cpp
+++ module_2/medianMaintenance/MedianMaintenance.cpp
@@ -1,30 +1,93 @@
+//============================================================================
+// Name        : MedianMaintenance.cpp
+// Author      : Veinstin Furtado
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Median Maintenance Algorithm using min and max heaps in C++, Ansi-style
+//============================================================================
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <array>
+#include <fstream>
 #include "Minheap.h"
 #include "Maxheap.h"
 
 using namespace std;
 
+
+
 int main() {
-	cout << "I will overcome." << endl;
-	vector<int> myHeap;
-	myHeap.push_back(9);
-	myHeap.push_back(14);
-	myHeap.push_back(5);
-	myHeap.push_back(4);
-	myHeap.push_back(8);
-	myHeap.push_back(12);
-	myHeap.push_back(1);
-	myHeap.push_back(7);
-	myHeap.push_back(10);
-	Minheap minhp(myHeap);
+	int linenumber = 0;
+	vector<int> heapvector;
+	Minheap minheap(heapvector);
+	Maxheap maxheap(heapvector);
+	int sumOfMedians  = 0;
+	int median = 0;
 
-	minhp.deleteNodeFromHeap(0);
-	minhp.deleteNodeFromHeap(2);
-	minhp.insertNodeIntoHeap(20);
-	minhp.insertNodeIntoHeap(30);
-	minhp.deleteNodeFromHeap(2);
-	minhp.printHeap();
+	string line;
+	int temp;
+	ifstream myfile("C:\\Users\\veins\\OneDrive\\Desktop\\Algorithms.git\\trunk\\module_2\\module_2_datasets\\median_maintenance_data_set.txt"); //median_maintenance_data_set
+	if (myfile.is_open())
+	{
+		while (getline(myfile, line))
+		{
+			temp = stoi(line, nullptr, 10);
+			//Call the Driver code for the Median Maintenance Algorithm
+			if ((maxheap.heap_vector_size == 0) || temp < maxheap.heap_vector[0]) { //if the number is lesser than the biggest(topmost) element in the max heap, insert it in the max heap
+					maxheap.insertNodeIntoHeap(temp);
+					if ((maxheap.heap_vector_size - minheap.heap_vector_size) > 1) {
+						minheap.insertNodeIntoHeap(maxheap.heap_vector[0]);
+						maxheap.deleteNodeFromHeap(0);
+					}
+			}
+			else if ((minheap.heap_vector_size == 0) || temp > minheap.heap_vector[0]) { //if the number is more than the smallest(topmost) element of the min heap, insert it in the min heap
+					minheap.insertNodeIntoHeap(temp);
+					if ((minheap.heap_vector_size - maxheap.heap_vector_size) > 1) {
+						maxheap.insertNodeIntoHeap(minheap.heap_vector[0]);
+						minheap.deleteNodeFromHeap(0);
+					}
+			}
+			else { //else you can put it in either heap(i chose the max heap)
+				maxheap.insertNodeIntoHeap(temp);
+				if ((maxheap.heap_vector_size - minheap.heap_vector_size) > 1) {
+					minheap.insertNodeIntoHeap(maxheap.heap_vector[0]);
+					maxheap.deleteNodeFromHeap(0);
+				}
+			}
+			
+			if ((maxheap.heap_vector_size % 2 == 0) && (minheap.heap_vector_size % 2 == 0)) { //both are even
+				median = maxheap.heap_vector[0];
+			}
 
+			if ((maxheap.heap_vector_size % 2 != 0) && (minheap.heap_vector_size % 2 != 0)) { //both are odd
+				median = maxheap.heap_vector[0];
+			}
+
+			if ((maxheap.heap_vector_size % 2 == 0) && (minheap.heap_vector_size % 2 != 0)) { //max heap size is even and min heap size is odd
+				if (maxheap.heap_vector_size > minheap.heap_vector_size)
+					median = maxheap.heap_vector[0];
+				else
+					median = minheap.heap_vector[0];
+			}
+
+			if ((maxheap.heap_vector_size % 2 != 0) && (minheap.heap_vector_size % 2 == 0)) { //max heap size is odd and min heap size is even
+				if (maxheap.heap_vector_size > minheap.heap_vector_size)
+					median = maxheap.heap_vector[0];
+				else
+					median = minheap.heap_vector[0];
+			}
+			sumOfMedians += median;
+		}
+
+		myfile.close();
+	}
+	else {
+		cout << "Unable to open file";
+	}
+
+	cout << "Final Result : " << sumOfMedians%10000 << endl;
 
 	return 0;
 }

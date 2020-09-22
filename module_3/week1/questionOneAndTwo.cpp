4726b2d9cd58da23a203fbb4c6551d9a1c1e356e
Veinstin Furtado <45301868+FurtadoDev@users.noreply.github.com>
2020-09-22 08:58:25 -0230

    Fixed bug for question 2

--- module_3/week1/questionOneAndTwo.cpp
+++ module_3/week1/questionOneAndTwo.cpp
@@ -19,22 +19,26 @@ using namespace std;
 
 
 typedef struct {
-	long long int job_weight;
-	long long int job_length;
-	long long int strategy;
+	float job_weight;
+	float job_length;
+	float strategy;
 } aTuple;
 
 
-vector<aTuple> *jobs;
+vector<aTuple>* jobs;
 void merge_sort(int l, int r);
 void merge(int l, int mid, int r);
 
 int main() {
 	int idx = 0;
-	int temp_int;
+	
+	float temp_int;
+	
 	int vector_size = 0;
+	
 	long long int sum = 0;
 	long long int comp_time = 0;
+
 	int len;
 	string line;
 	stringstream str_strm;
@@ -58,22 +62,22 @@ int main() {
 			theTuple.job_weight = temp_int;
 			str_strm >> temp_int;
 			theTuple.job_length = temp_int;
-			theTuple.strategy = (theTuple.job_weight) - (theTuple.job_length); //change the strategy according to the question
+			theTuple.strategy = (theTuple.job_weight) / (theTuple.job_length); //change the strategy according to the question
 			jobs->operator[](idx) = theTuple;
 			idx++;
 			str_strm.clear();
 		}
-			myfile.close();
+		myfile.close();
 	}
 	else {
 		cout << "Cannot open file" << endl;
 	}
 
 
-	merge_sort(0, vector_size-1);
+	merge_sort(0, vector_size - 1);
 	for (int i = 0; i < vector_size; i++) {
-		comp_time = comp_time + (jobs->operator[](i).job_length);
-		sum = sum + ((jobs->operator[](i).job_weight) * comp_time);
+		comp_time = comp_time + ((int)jobs->operator[](i).job_length);
+		sum = sum + ((int)(jobs->operator[](i).job_weight) * comp_time);
 		/*
 		cout << jobs->operator[](i).job_weight << endl;
 		cout << jobs->operator[](i).job_length << endl;
@@ -82,7 +86,7 @@ int main() {
 		*/
 	}
 	cout << "Answer : " << sum << endl;
-	
+
 	delete jobs;
 	return 0;
 }
@@ -91,15 +95,15 @@ int main() {
 void merge_sort(int l, int r) {
 	int mid;
 	if (l < r) {
-		mid = (l+r) / 2;
+		mid = (l + r) / 2;
 		merge_sort(l, mid);
-		merge_sort(mid+1, r);
+		merge_sort(mid + 1, r);
 		merge(l, mid, r);
 	}
 }
 
 void merge(int l, int mid, int r) {
-	vector<aTuple>* temp = new vector<aTuple>(r-l+1);
+	vector<aTuple>* temp = new vector<aTuple>(r - l + 1);
 	int ptr1 = l;
 	int ptr2 = mid + 1;
 	int idx = 0;

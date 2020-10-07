da06ff1006c0994d60bd3f700f69c0680fac239e
Veinstin Furtado <45301868+FurtadoDev@users.noreply.github.com>
2020-10-07 15:15:37 -0230

    Added comments

--- module_3/week1/question3/Graph.h
+++ module_3/week1/question3/Graph.h
@@ -1,3 +1,23 @@
+/*!
+ * @file      Graph.h
+ * @brief     Declaration of the Graph class.
+ *
+ * @author    Veinstin Furtado <vrfurtado@mun.ca>
+ * @copyright (c) 2020 Veinstin Furtado. All rights reserved.
+ * @license   Apache License, Version 2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy
+ * of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
 #pragma once
 #include <iostream>
 #include <vector>
@@ -9,9 +29,13 @@ class Graph
 
 public:
 	int num_vertices;
+	int num_edges;
 	list<pair<int, int>>* adjList;
-
-	Graph(int num_vertices);
+	/**
+	 * Constructor which takes in the number of vertices and the number of edges in the Graph. An adjacency list is initialized using the number of vertices passed here.
+	 *
+	 */
+	Graph(int num_vertices, int num_edges);
 
 	void addEdge(int u, int v, int w);
 };

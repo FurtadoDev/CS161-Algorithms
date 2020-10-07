05b7b3c3b6e16916266d0a6aa2adad2e4d6714c3
Veinstin Furtado <45301868+FurtadoDev@users.noreply.github.com>
2020-10-07 15:14:31 -0230

    Updated the function that adds an edge to the graph

--- module_3/week1/question3/Graph.cpp
+++ module_3/week1/question3/Graph.cpp
@@ -1,12 +1,13 @@
 #include "Graph.h"
 
-Graph::Graph(int num_vertices)
+Graph::Graph(int num_vertices, int num_edges)
 {
 	this->num_vertices = num_vertices;
-	this->adjList = new list<pair<int, int>>[num_vertices];
+	this->num_edges = num_edges;
+	this->adjList = new list<pair<int, int>>[num_vertices+1]; //not using the 0th position to make things simpler
 }
 
 void Graph::addEdge(int u, int v, int w) {
 	this->adjList[u].push_back(make_pair(v, w));
-	//this->adjList[v].push_back(make_pair(u, w)); PIECE OF ADVICE : DON'T GET AHEAD OF YOURSELF
+	this->adjList[v].push_back(make_pair(u, w));
 }
